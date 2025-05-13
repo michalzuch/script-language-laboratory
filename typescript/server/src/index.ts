7592344fe5a1fe155db576919973de654ab5354e
michał <51276321+michalzuch@users.noreply.github.com>
2025-05-13 23:24:13 +0200

    Add CORS configuration

--- typescript/server/src/index.ts
+++ typescript/server/src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from 'elysia'
 import { router } from './routes'
 
-const app = new Elysia().use(router).listen(3000)
+const app = new Elysia()
+  .use(router)
+  .onRequest(({ set }) => {
+    set.headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
+    set.headers['Access-Control-Allow-Methods'] = 'GET'
+    set.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    set.headers['Access-Control-Allow-Credentials'] = 'true'
+  })
+  .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
